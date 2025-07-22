35b4841d7080776a16bd3edf43e3b7a3a72d2171
Temitope <jerrytope009@gmail.com>
2025-07-22 08:43:49 +0100

    yes

--- NICKY_COSMETICS/settings.py
+++ NICKY_COSMETICS/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = config('SECRET_KEY', default="django-insecure-1a2u!a$o*gno%r^4k*6xz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
 
-
+ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
